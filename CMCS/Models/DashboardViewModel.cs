f1904f166d9a3956a5c9984a689e6536898db02c
MachakaireAshley <ST10443342@imconnect.edu.za>
2025-09-15 09:41:41 +0200

    Initial Push
    this is my first push showing my Models and placing the Initial document onto GitHub

---
+++ CMCS/Models/DashboardViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace CMCS.Models
+{
+    public class DashboardViewModel
+    {
+        public int PendingClaims { get; set; }
+        public int RejectedClaims { get; set; }
+        public int AcceptedClaims { get; set; }
+        public List<Claim> RecentClaims { get; set; }
+        public List<Notification> Notifications { get; set; }
+        public List<Message> Messages { get; set; }
+    }
+}
